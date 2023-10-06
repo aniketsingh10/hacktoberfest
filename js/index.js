5396f114f6fb3385567550ecafa23345fb8a01c8
aniketsingh10 <aniketsinghofficial144@gmail.com>
2023-10-06 20:32:45 +0530

    updated the functionality files

---
+++ js/index.js
@@ -0,0 +1,54 @@
+let obj = [{ a: 1 }, { a: 2 }, { a: 3 }, { a: 2 }, { a: 5 }];
+
+let findnum = obj.find((num) => num.a === 2);
+
+console.log(findnum);
+
+// let obj1={
+//   a:10,
+//   b:12,
+// }
+
+// let obj2=obj1;
+// obj1.a+=1;
+
+// function print(start, end) {
+//   let arr = [];
+//   for (let i = start; i <= end; i++) {
+//     arr[i] = i;
+//   }
+//   return arr;
+// }
+
+// let obj1 = print(10, 100);
+//var a = 10;
+// {
+//     let a=15;
+//     console.log(a);
+// }
+//let a=19;
+// a = "Aniket";
+// console.log(a);
+
+// let cricket = {
+//   ball: "tenis",
+//   bat: "sg",
+// };
+
+// console.log(cricket);
+
+// let num=3;
+// let p=(num>=18)?"I can vote":"I cannot vote";
+// console.log(false || 5 || 6 || 13);
+
+// function rectangle(a, b) {
+//   return {
+//     breath: a,
+//     lenght: b,
+//     area: a * b,
+//   };
+// }
+
+// let ans = rectangle(10, 12);
+// console.log(ans.area);
+// let ans1=rectangle(30,40);
